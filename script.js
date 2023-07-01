87eeab010c8ae35f3b47e0bd9dd058a159fd1e0d
Ayrton Santillana <ayrtontv@gmail.com>
2023-06-30 22:17:19 -0600

    Se crea la función para que la computadora elija de manera aleatoria, el usuario escoja y se juege

---
+++ script.js
@@ -0,0 +1,28 @@
+const user = prompt("¿Cuál es tu nombre?");
+let playerSelection = prompt("Escoge: Piedra, Papel o Tijeras");
+playerSelection = playerSelection.toLowerCase();
+const computerSelection = getComputerChoice();
+
+console.log(playerSelection);
+
+function getComputerChoice(){
+    const options = ["piedra", "papel", "tijeras"];
+    return options[Math.floor(Math.random() * options.length)];
+}
+console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection){
+    if(playerSelection == "piedra" && computerSelection == "tijeras"
+    || playerSelection == "papel" && computerSelection == "piedra"
+    || playerSelection == "tijeras" && computerSelection == "papel"){
+        console.log(`Felicidades, ${user}! ${playerSelection} le gana a ${computerSelection}`);
+    } else if(computerSelection == "piedra" && playerSelection == "tijeras"
+    || computerSelection == "papel" && playerSelection == "piedra"
+    || computerSelection == "tijeras" && playerSelection == "papel"){
+        console.log(`Lo siento, ${user}! ${computerSelection} le gana a ${playerSelection}`);
+    } else{
+        console.log("Es un empate");
+    }
+}
+
+playRound();
