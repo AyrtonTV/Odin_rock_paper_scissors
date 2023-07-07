de0f53048e8a960486e3452e399194fd9464502d
Ayrton Santillana <ayrtontv@gmail.com>
2023-07-06 20:04:11 -0600

    Se terminó el bucle que jugaba 5 veces playRound() Además de implementar un contador de puntos y una condicional if para mostrar al ganador después de la ronda de 5 juegos

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Se obtiene la información del user
 const user = prompt("¿Cuál es tu nombre?");
+let playerSelection = "";
+let computerSelection = "";
 
 function playerChoice(){
     let opcion = prompt("Escoge: Piedra, Papel o Tijeras");
@@ -10,12 +12,11 @@ function getComputerChoice(){
     const options = ["piedra", "papel", "tijeras"];
     return options[Math.floor(Math.random() * options.length)];
 }
-const juego = playRound();
 
 // Se juega una ronda
 function playRound(){
-    const playerSelection = playerChoice();
-    const computerSelection = getComputerChoice();
+    playerSelection = playerChoice();
+    computerSelection = getComputerChoice();
 
     console.log(playerSelection);
     console.log(computerSelection);
@@ -23,26 +24,61 @@ function playRound(){
     if(playerSelection == "piedra" && computerSelection == "tijeras"
     || playerSelection == "papel" && computerSelection == "piedra"
     || playerSelection == "tijeras" && computerSelection == "papel"){
-        return 1, 
-        console.log(`Felicidades, ${user}! ${playerSelection} le gana a ${computerSelection}`);
+        return 1;
+        // console.log(`Felicidades, ${user}! ${playerSelection} le gana a ${computerSelection}`);
     } else if(computerSelection == "piedra" && playerSelection == "tijeras"
     || computerSelection == "papel" && playerSelection == "piedra"
     || computerSelection == "tijeras" && playerSelection == "papel"){
-        return 2,
-        console.log(`Lo siento, ${user}! ${computerSelection} le gana a ${playerSelection}`);
+        return 2;
+        // console.log(`Lo siento, ${user}! ${computerSelection} le gana a ${playerSelection}`);
     } else{
-        return 3,
-        console.log(`Es un empate, ambos eligieron ${playerSelection}`);
+        return 3;
+        // console.log(`Es un empate, ambos eligieron ${playerSelection}`);
     }
 }
 
-let playerScore = +("");
-let computerScore = +("");
+let playerScore = 0;
+let computerScore = 0;
 
 //Se juega una ronda de 5
 function game(){
-    for(i = 1; i <= 5; i++){
-        playRound();
+    for(i = 0; i < 5; i++){
+        let playRound5 = playRound();
+        if(playRound5 === 1){
+            console.log(`Felicidades, ${user}! ${playerSelection} le gana a ${computerSelection}. Has ganado un punto.`);
+            playerScore += 1;
+            console.log(`Marcador: ${user}: ${playerScore}
+            Computadora: ${computerScore}`);
+        }
+        else if(playRound5 === 2){
+            console.log(`Lo siento, ${user}! ${computerSelection} le gana a ${playerSelection}. Computadora gana un punto.`);
+            computerScore += 1;
+            console.log(`Marcador: ${user}: ${playerScore}
+            Computadora: ${computerScore}`);
+        }
+        else if(playRound5 === 3){
+            console.log(`Es un empate, ambos eligieron ${playerSelection}. Nadie gana puntos.`);
+            console.log(`Marcador: ${user}: ${playerScore}
+            Computadora: ${computerScore}`);
+        }
+    }
+    console.log(`
+    ******************************
+    
+            Marcador final      
+            ${user}: ${playerScore}      
+            Computadora: ${computerScore}     
+                             
+    ******************************
+                                                        `);
+    if(playerScore === computerScore){
+        console.log(`Es un empate, ambos obtuvieron ${playerScore}`);
+    }
+    else if(playerScore > computerScore){
+        console.log(`Felicidades, ${user}!, has ganado la partida!`);
+    }
+    else{
+        console.log(`Lo siento, ${user}, la Computadora te ha ganado`);
     }
 }
 
