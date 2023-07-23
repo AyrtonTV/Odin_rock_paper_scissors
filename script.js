ed88e926336f1790a71165ea793db083942caf53
Ayrton Santillana <ayrtontv@gmail.com>
2023-07-22 23:17:31 -0600

    Se agregaron los botones para cada opcion ademas de los event listeners

--- script.js
+++ script.js
@@ -1,42 +1,56 @@
 // Se obtiene la información del user
-const user = prompt("¿Cuál es tu nombre?");
+//const user = prompt("¿Cuál es tu nombre?");
 let playerSelection = "";
 let computerSelection = "";
 
-function playerChoice(){
-    let opcion = prompt("Escoge: Piedra, Papel o Tijeras");
-    return opcion.toLowerCase();
+const piedra = document.querySelector(".rock");
+const tijeras = document.querySelector(".scissors");
+const papel = document.querySelector(".paper");
+
+const boton = document.querySelector(".btn");
+
+function selector(){
+    console.log(boton.getAttribute("class"));
 }
 
-function getComputerChoice(){
-    const options = ["piedra", "papel", "tijeras"];
-    return options[Math.floor(Math.random() * options.length)];
-}
+piedra.addEventListener("click", selector);
+tijeras.addEventListener("click", selector);
+papel.addEventListener("click", selector);
 
-// Se juega una ronda
-function playRound(){
-    playerSelection = playerChoice();
-    computerSelection = getComputerChoice();
+// function playerChoice(){
+//     let opcion = prompt("Escoge: Piedra, Papel o Tijeras");
+//     return opcion.toLowerCase();
+// }
 
-    console.log(playerSelection);
-    console.log(computerSelection);
+// function getComputerChoice(){
+//     const options = ["piedra", "papel", "tijeras"];
+//     return options[Math.floor(Math.random() * options.length)];
+// }
 
-    if(playerSelection == "piedra" && computerSelection == "tijeras"
-    || playerSelection == "papel" && computerSelection == "piedra"
-    || playerSelection == "tijeras" && computerSelection == "papel"){
-        return 1;
-        // console.log(`Felicidades, ${user}! ${playerSelection} le gana a ${computerSelection}`);
-    } else if(computerSelection == "piedra" && playerSelection == "tijeras"
-    || computerSelection == "papel" && playerSelection == "piedra"
-    || computerSelection == "tijeras" && playerSelection == "papel"){
-        return 2;
-        // console.log(`Lo siento, ${user}! ${computerSelection} le gana a ${playerSelection}`);
-    } else{
-        return 3;
-        // console.log(`Es un empate, ambos eligieron ${playerSelection}`);
-    }
-}
+// // Se juega una ronda
+// function playRound(){
+//     playerSelection = playerChoice();
+//     computerSelection = getComputerChoice();
 
-let playerScore = 0;
-let computerScore = 0;
+//     console.log(playerSelection);
+//     console.log(computerSelection);
+
+//     if(playerSelection == "piedra" && computerSelection == "tijeras"
+//     || playerSelection == "papel" && computerSelection == "piedra"
+//     || playerSelection == "tijeras" && computerSelection == "papel"){
+//         return 1;
+//         // console.log(`Felicidades, ${user}! ${playerSelection} le gana a ${computerSelection}`);
+//     } else if(computerSelection == "piedra" && playerSelection == "tijeras"
+//     || computerSelection == "papel" && playerSelection == "piedra"
+//     || computerSelection == "tijeras" && playerSelection == "papel"){
+//         return 2;
+//         // console.log(`Lo siento, ${user}! ${computerSelection} le gana a ${playerSelection}`);
+//     } else{
+//         return 3;
+//         // console.log(`Es un empate, ambos eligieron ${playerSelection}`);
+//     }
+// }
+
+// let playerScore = 0;
+// let computerScore = 0;
 
